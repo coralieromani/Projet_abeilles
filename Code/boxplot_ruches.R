f2e7e3b9185e2e1eabac9924d4c0d202d217685e
mgermain12 <marine.germain@etu.umontpellier.fr>
2025-03-30 16:43:43 +0200

    séparation boxplot sica et confoux

--- Code/boxplot_ruches.R
+++ Code/boxplot_ruches.R
@@ -6,19 +6,40 @@ library(readxl)
 # Charger les données
 poids_ruches <- read_excel("BD/Weight_2023_Sica_Confoux.xlsx")
 
-# Exclure la colonne de date et ne garder que les colonnes des poids des ruches
-poids_ruches_sans_date <- poids_ruches %>%
-  select(-date)  # Exclure la colonne 'date'
+colnames(poids_ruches) <- c("date", "Confoux - 412", "Confoux - 409", "Confoux - 435",
+                            "Confoux - 430", "Confoux - 404", "Confoux - 405",
+                            "Sica - 415", "Sica - 428", "Sica - 444",
+                            "Sica - 412", "Sica - Y", "Sica - 89")
+
+poids_sansdate <- poids_ruches %>%
+  select(-1)
+
+#Suppression de la colonne Date et des rûches de Confoux
+poids_sica_sansdate <- poids_sansdate %>%
+  select((ncol(poids_sansdate)-5):ncol(poids_sansdate))
+
+#Suppression de la colonne Date et des rûches de Sica
+poids_confoux_sansdate <- poids_sansdate %>%
+  select(1:6)
 
 # Transformer les données en format long
-poids_long <- poids_ruches_sans_date %>%
+
+poids_long_confoux <- poids_confoux_sansdate%>%
   pivot_longer(cols = everything(), names_to = "ruche", values_to = "poids") %>%
   filter(!is.na(poids))  # Filtrer les valeurs manquantes
 
-# Créer le boxplot pour chaque ruche
-ggplot(poids_long, aes(x = ruche, y = poids)) +
+poids_long_sica <- poids_sica_sansdate%>%
+  pivot_longer(cols = everything(), names_to = "ruche", values_to = "poids") %>%
+  filter(!is.na(poids))  # Filtrer les valeurs manquantes
+
+#Tracé des boxplot
+ggplot(poids_long_confoux, aes(x = ruche, y = poids)) +
   geom_boxplot(fill = "pink", color = "purple") +
-  labs(title = "Boxplot des poids des ruches", y = "Poids")
+  labs(y = "Poids", x ="Rûches")
+
+ggplot(poids_long_sica, aes(x = ruche, y = poids)) +
+  geom_boxplot(fill = "pink", color = "purple") +
+  labs(y = "Poids", x = "Rûches")
 
 # mauve = médiane
 # moustache = étendue des données en dehors des quantiles
