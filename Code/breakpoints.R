6cee8000d293cff68c4f70c857ef36a78c387ae8
mgermain12 <marine.germain@etu.umontpellier.fr>
2025-04-13 15:44:11 +0200

    modif graphe pour rapport

--- Code/breakpoints.R
+++ Code/breakpoints.R
@@ -78,19 +78,30 @@ for (i in unique(poids_moyen$date)) {
 breakpoints_table <- breakpoints_table %>%
   slice(-1)
 
-test <- subset(poids_moyen, date == "2023-05-10")
+test <- subset(poids_moyen, date == "2023-04-22")
 modele <- lm(poids ~ heure, data = test)
-modele_seg <- segmented(modele, seg.Z = ~heure, npsi = 2)
-plot(test$heure, test$poids, type = "b", main = paste("Régression segmentée le 2023-05-06"), xlab = "Heure", ylab = "Poids")
-lines(test$heure, predict(modele_seg), col = "red", lwd = 2)
+modele_seg <- segmented(modele, seg.Z = ~heure, npsi = 4)
+
+test$heure_POSIX <- as.POSIXct(sprintf("%02d:%02d", test$heure %/% 60, test$heure %% 60), format = "%H:%M")
+plot(test$heure_POSIX, test$poids, type = "b",
+     xlab = "Heure", ylab = "Poids", 
+     xaxt = "n")  # Supprimer l'axe X par défaut
+axis.POSIXct(1, at = pretty(test$heure_POSIX), format = "%H:%M")
+lines(test$heure_POSIX, predict(modele_seg), col = "red", lwd = 2)
 
 for (i in jours_erreurs) {
-  erreur <- subset(poids_moyen, date == i)
+  erreur <- subset(poids_moyen, date == as.Date(i))  
   modele <- lm(poids ~ heure, data = erreur)
   modele_seg <- segmented(modele, seg.Z = ~heure, npsi = 1)
-  plot(erreur$heure, erreur$poids, type = "b", main = paste("Régression segmentée le", i), xlab = "Heure", ylab = "Poids")
-  lines(erreur$heure, predict(modele_seg), col = "red", lwd = 2)
+  erreur$heure_POSIX <- as.POSIXct(sprintf("%02d:%02d", erreur$heure %/% 60, erreur$heure %% 60), format = "%H:%M", tz = "UTC")
+  plot(erreur$heure_POSIX, erreur$poids, type = "b", 
+       #main = paste("Régression segmentée le", i),
+       xlab = "Heure", ylab = "Poids", xaxt = "n")
+  
+  axis.POSIXct(1, at = pretty(erreur$heure_POSIX), format = "%H:%M", tz = "UTC")
+  lines(erreur$heure_POSIX, predict(modele_seg), col = "red", lwd = 2)
 }
+
 
 
 ggplot(breakpoints_table) +
@@ -101,41 +112,46 @@ ggplot(breakpoints_table) +
   scale_color_manual(values = c("BP_1" = "blue", "BP_2" = "orange"), name = "Breakpoints") +
   theme_minimal()
 
-#Conversion des heures:minutes en heures (ex: 07:30 devient 7,5)
+# Conversion des heures:minutes en heures
 BP_1_numeric <- as.numeric(substr(breakpoints_table$BP_1, 1, 2)) +
   as.numeric(substr(breakpoints_table$BP_1, 4, 5)) / 60
 BP_2_numeric <- as.numeric(substr(breakpoints_table$BP_2, 1, 2)) +
   as.numeric(substr(breakpoints_table$BP_2, 4, 5)) / 60
 
+breakpoints_table$BP_1_numeric <- as.numeric(substr(breakpoints_table$BP_1, 1, 2)) +
+  as.numeric(substr(breakpoints_table$BP_1, 4, 5)) / 60
+breakpoints_table$BP_2_numeric <- as.numeric(substr(breakpoints_table$BP_2, 1, 2)) +
+  as.numeric(substr(breakpoints_table$BP_2, 4, 5)) / 60
+
+# Régressions
 lm_BP1 <- lm(BP_1_numeric ~ as.numeric(date), data = breakpoints_table)
 lm_BP2 <- lm(BP_2_numeric ~ as.numeric(date), data = breakpoints_table)
 
-# Calcul du R2
-R2_BP1 <- summary(lm_BP1)$r.squared
-R2_BP2 <- summary(lm_BP2)$r.squared
+# R²
+R2_BP1 <- round(summary(lm_BP1)$r.squared, 3)
+R2_BP2 <- round(summary(lm_BP2)$r.squared, 3)
 
-#Droites de régression pour les breakpoints 1 et 2
-BP_1_trend <- predict(lm_BP1, newdata = breakpoints_table)
-BP_2_trend <- predict(lm_BP2, newdata = breakpoints_table)
+# Droites de régression
+breakpoints_table$BP_1_trend <- predict(lm_BP1, newdata = breakpoints_table)
+breakpoints_table$BP_2_trend <- predict(lm_BP2, newdata = breakpoints_table)
 
-# Calcul d'une position pour placer les R² (90% du range de dates)
-date_range <- range(breakpoints_table$date)
-annot_date <- date_range[1] + 0.9 * as.numeric(difftime(date_range[2], date_range[1], units = "days"))
+# Position pour les annotations
+max_date <- max(breakpoints_table$date)
+min_date <- min(breakpoints_table$date)
+annot_x <- min_date + (max_date - min_date) * 0.7
 
+# Graphique
 ggplot(breakpoints_table, aes(x = date)) +
   geom_point(aes(y = BP_1_numeric, color = "Breakpoint 1")) +
   geom_point(aes(y = BP_2_numeric, color = "Breakpoint 2")) +
-  geom_line(aes(y = BP_1_trend), linetype = "solid", col = 'darkgreen', size=1) +
-  geom_line(aes(y = BP_2_trend), linetype = "solid", col = "#D5006D", size=1) +
-  labs(
-    #title = "Comparaison des breakpoints 1 et 2", 
-    x = "Jour", y = "Heure") +
+  geom_line(aes(y = BP_1_trend), color = "darkgreen", size = 1) +
+  geom_line(aes(y = BP_2_trend), color = "#D5006D", size = 1) +
+  annotate("text", x = annot_x, y = 23.5, label = paste("R² BP1 =", R2_BP1), color = "darkgreen", hjust = 0) +
+  annotate("text", x = annot_x, y = 22.5, label = paste("R² BP2 =", R2_BP2), color = "#D5006D", hjust = 0) +
+  labs(x = "Jour", y = "Heure") +
   scale_y_continuous(breaks = seq(0, 24, by = 1), limits = c(0, 24)) +
   scale_color_manual(values = c("Breakpoint 1" = "green", "Breakpoint 2" = "pink"), name = "Breakpoints") +
-  theme_minimal() +
-  # Ajouter les R2 sur le graphique
-  annotate("text", x = max(breakpoints_table$date) * 0.9, y = max(BP_1_numeric), label = paste("R² BP1 = ", round(R2_BP1, 2)), color = "darkgreen", hjust = 0) +
-  annotate("text", x = max(breakpoints_table$date) * 0.9, y = max(BP_2_numeric), label = paste("R² BP2 = ", round(R2_BP2, 2)), color = "#D5006D", hjust = 0)
+  theme_minimal()
 
 #Explication des ajouts :
 
@@ -161,6 +177,11 @@ ggplot(breakpoints_table, aes(x = date, y = variation_poids)) +
                                   max(breakpoints_table$variation_poids),
                                   by = 1)) +
   theme_minimal()
+
+modele_pente <- lm(variation_poids ~ as.numeric(date), data = breakpoints_table_filtre)
+
+# Résumé du modèle pour extraire le R²
+R2_pente = summary(modele_pente)$r.squared
 
 #REMARQUES: variations étonnantes pour certaine journées
 #le 10/05/2023 : -2,46g  (1h01 et 3g environ)
